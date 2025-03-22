435c1e8cabc0a6a05d09716ce266d4f590aa95e0
Hardik-Mehta-Python <hardik.mehta212005@gmail.com>
2025-03-22 14:00:33 +0530

    models

--- emp/models.py
+++ emp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+class Role(models.Model):
+    name = models.CharField(max_length=100, null=False)
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100,null=False)
+    dept = models.ForeignKey(Department,on_delete=models.CASCADE)
+    salary = models.IntegerField(default=0)
+    bonus = models.IntegerField(default=0)
+    role = models.ForeignKey(Role,on_delete=models.CASCADE)
+    phone_number = models.IntegerField(max_length=10)
+    hire_date = models.DateField()
